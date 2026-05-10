3807c443ff70adf343377c66bbe1831cee59f9b7
Ahmed <ahmaddeiri4@gmail.com>
2026-05-10 18:39:00 +0300

    Brevo mail entegrasyonu, email doğrulama ve şifre sıfırlama eklendi

---
+++ TeacherFind.Infrastructure/Services/Email/EmailOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherFind.Infrastructure.Services.Email;
+
+public class EmailOptions
+{
+    public string Provider { get; set; } = "Brevo";
+    public string ApiKey { get; set; } = default!;
+    public string FromEmail { get; set; } = default!;
+    public string FromName { get; set; } = "TeacherFind";
+}
